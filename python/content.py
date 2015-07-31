b01a39a0f4758fc97b2770aade0c8aaf81601cb0
Robert Clark <robertclark0@gmail.com>
2015-07-30 23:50:47 -0500

    Major update. Added review modal and associated html and styling

--- python/content.py
+++ python/content.py
@@ -31,7 +31,8 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_review=movie.storyline
         )
     return content
 
